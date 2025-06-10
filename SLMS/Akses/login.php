1b321260bec2543551fd96e3610496aebe814da8
Alythar <nisrinaaliya05@gmail.com>
2025-06-10 13:18:39 +0700

    add text

--- SLMS/Akses/login.php
+++ SLMS/Akses/login.php
@@ -19,7 +19,7 @@
     <!-- Navigasi Logo Start -->
     <nav>
         <div class="logo">
-            <p>HAI</p>
+            <p>SLMS</p>
             <p>Student Loan Management System</p>
         </div>
 
